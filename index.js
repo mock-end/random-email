8d24167de74117b408115ce5747111b5617984f8
bubkoo <bubkoo.wy@gmail.com>
2016-04-18 10:27:07 +0800

    v1.0.1

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 
+var toString      = require('to-str');
 var randomLorem   = require('random-lorem');
 var randomDoamins = require('random-domains');
 
 
-module.exports = function () {
-  return randomLorem() + '@' + randomDoamins();
+module.exports = function (domain) {
+
+  domain = domain && toString(domain) || randomDoamins();
+
+  return randomLorem() + '@' + domain;
 };
--- test/spec/index.js
+++ test/spec/index.js
@@ -9,4 +9,8 @@ describe('random-email: ', function () {
     expect(randomEmail()).to.be.a('string');
     expect(randomEmail()).to.be.match(/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/);
   });
+
+  it('randomEmail("random.com")', function () {
+    expect(randomEmail('random.com')).to.be.match(/random\.com$/);
+  });
 });
